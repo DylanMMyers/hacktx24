d19548dbaf829575c5ecff77bcfa0dd3cefca6d0
DylanMMyers <135419290+DylanMMyers@users.noreply.github.com>
2024-11-02 23:22:34 -0500

    fixed algo

--- data/s3req.py
+++ data/s3req.py
@@ -8,6 +8,20 @@ def req(url):
     df = pd.read_csv(io.StringIO(csv_content))
     return df
 
+def interestsmatch(criteria, ele):
+    for x in ele["interests"]:
+        if ele["interests"] in criteria["interests"]:
+            return True
+    return False
+
+def costfind(df, cost):
+    for ele in df:
+        if ele["cost"] != cost:
+            continue
+        if (ele not in criteria["dont"]) and (interestsmatch(criteria, ele) or ele["rating"] > 7):
+            return ele
+    return -1
+
 def findf(url, dont, criteria):
     df = req(url)
     # then search through df for optimal event of ___ category
@@ -15,30 +29,32 @@ def findf(url, dont, criteria):
     # if found date meets criteria, we can try to select it
     # cost must be met, interest should be met, rating high should be met but interest takes prio
     
-    interestsmatch = None
+    # df should contain ele objects, these will have qualities:
+    # Category, Name, Price, Distance to Nearest Train Station, Ratings, Nearest Train Station
     
+    # criteria should be a dict with attributes:
+    # cost, days, interests, dont
+
     if criteria["cost"] == "low":
-        # placeholders
-        for ele in df:
-            if (ele not in dont) and (interestsmatch is True):
-                return ele
-        a = 1
+        newele = costfind(df, "low")
     elif criteria["cost"] == "mid":
-        b = 1
+        newele = costfind(df, "mid")
     else:
-        c = 1
+        newele = costfind(df, "high")
 
-
+    if newele == -1:
+        return "Not found"
+    else:
+        return newele
 
 if __name__ == "__main__":
     city = "osaka"
     category = "placestoeat"
     url = f"https://nihangodb.s3.amazonaws.com/{city}_{category}.csv"
-    dont = [] # names of events to not use
     criteria = {
         "cost" : "",
         "days" : "",
-        "ct" : 0, # avg commute time
-        "interests" : []
+        "interests" : [],
+        "dont" : [] # names of events to not use
     }
-    findf(url, dont, criteria, category)
+    findf(url, criteria, category)
